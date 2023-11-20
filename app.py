d75a9872cb569f387cb8a03282ba7ab1ef45dd3d
jacob-sasser <44883537+jacob-sasser@users.noreply.github.com>
2023-11-20 14:00:49 -0500

    added log in, about pages

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask, abort, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 @app.route('/')
-def index():
-    return"test"
+def home():
+    return render_template("home.html")
 
+@app.route('/login')
+def login():
+    return render_template("logIn.html")
+@app.route('/about')
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
