afac3cd85552eb6767255950dc000b6a34d81638
Sushant23456 <143038879+Sushant23456@users.noreply.github.com>
2023-11-28 14:56:43 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,59 +1,37 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, ForeignKey 
-from datetime import datetime
 
-db= SQLAlchemy()
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(80))
-    #pfp=Column(db.String)
-    business=Column(db.Boolean(), default=False, nullable=False)
+    business = db.Column(db.Boolean, default=False)
 
-    def getUser(self):
-        return User.query.get(self.user_id)
-    def isBusiness(self):
-        return User.query.get(self.business)
-    
     def __repr__(self):
-        return '<User %r>' % self.username
-    
+        return f'<User {self.name}>'
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    post_id = Column(db.Integer, primary_key=True)
-    user_id=Column(db.Integer, ForeignKey('users.user_id'))
-    title = Column(db.String(80))
-    body = Column(db.String())
-    timestamp =Column(db.String())
-    byBusiness = Column(db.Boolean, ForeignKey('users.business'))
-    
-    
-    def getUser(self):
-        return Post.query.get(self.user_id)
-    
-    def getPost(self):
-        return Post.query.get(self.post_id)
-    
-    def getTimestamp(self):
-        return Post.query.get(self.timestamp)
-    
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    title = db.Column(db.String(80))
+    body = db.Column(db.String())
+    timestamp = db.Column(db.DateTime)
+    byBusiness = db.Column(db.Boolean)
 
-    def isBusiness(self):
-        return Post.query.get(self.byBusiness)
-    def getRepliesCount(self):
-        return Post.query.get(self.repliesCount)
+    def __repr__(self):
+        return f'<Post {self.title}>'
 
-    
 class Reply(db.Model):
     __tablename__ = 'replies'
-    user_id=Column(db.Integer, ForeignKey('User.user_id'))
-    post_id=Column(db.Integer, ForeignKey('posts.post_id'))
-    reply_id=Column(db.Integer, nullable=False, primary_key=True)
-    body= Column(db.String, nullable = False)
-    timestamp =Column(db.String())
+    reply_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
+    body = db.Column(db.String())
+    timestamp = db.Column(db.DateTime)
 
+    def __repr__(self):
+        return f'<Reply {self.reply_id}>'
