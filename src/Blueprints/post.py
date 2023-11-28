83787eeca8f950e5fec8d50f92e9d92885845bef
jacob-sasser <44883537+jacob-sasser@users.noreply.github.com>
2023-11-28 10:23:33 -0500

    bug fixes

--- src/Blueprints/post.py
+++ src/Blueprints/post.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, abort, redirect, render_template, request, session, url_for
 import sqlalchemy
 from datetime import datetime, timedelta
-from models import Post, User, db, Reply
+
+from src.models import Post, db, Reply
 
 
 
--- src/models.py
+++ src/models.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column
+from sqlalchemy import Column, ForeignKey 
 from datetime import datetime
 
 db= SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(80))
-    pfp=db.Column(db.String)
-    business=db.Column(db.Boolean(), default=False, nullable=False)
+    user_id = Column(db.Integer, nullable=False, primary_key=True)
+    name = Column(db.String(50))
+    email = Column(db.String(120), unique=True)
+    password = Column(db.String(80))
+    pfp=Column(db.String)
+    business=Column(db.Boolean(), default=False, nullable=False)
 
     def getUser(self):
         return User.query.get(self.user_id)
@@ -25,12 +25,12 @@ class User(db.Model):
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    post_id = db.Column(db.Integer, primary_key=True)
-    user_id=db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    title = db.Column(db.String(80))
-    body = db.Column(db.String())
-    timestamp =db.Column(db.String())
-    byBusiness = db.Column(db.Boolean, db.ForeginKey('User.business'))
+    post_id = Column(db.Integer, primary_key=True)
+    user_id=Column(db.Integer, ForeignKey('users.user_id'))
+    title = Column(db.String(80))
+    body = Column(db.String())
+    timestamp =Column(db.String())
+    byBusiness = Column(db.Boolean, ForeignKey('users.business'))
     
     
     def getUser(self):
@@ -51,9 +51,9 @@ class Post(db.Model):
     
 class Reply(db.Model):
     __tablename__ = 'replies'
-    user_id=db.Column(db.Integer, db.ForeignKey('User.user_id'))
-    post_id=db.Column(db.Integer, db.ForeignKey('posts.post_id'))
-    reply_id=db.Column(db.Integer, nullable=False, primary_key=True)
-    body= db.Column(db.String, nullable = False)
-    timestamp =db.Column(db.String())
+    user_id=Column(db.Integer, ForeignKey('User.user_id'))
+    post_id=Column(db.Integer, ForeignKey('posts.post_id'))
+    reply_id=Column(db.Integer, nullable=False, primary_key=True)
+    body= Column(db.String, nullable = False)
+    timestamp =Column(db.String())
 
